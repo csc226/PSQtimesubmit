47662213f5e566b0c41add9e1ab92a5f88245daa
css226 <1435366285@qq.com>
2022-05-27 09:48:57 +0800

    gx

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd /d D:\2022\霍家营街道\问卷\dingshi&&title 接口服务&&node app.js" 
-start cmd /k "cd/d D:\2022\霍家营街道\问卷\dingshi&&title 页面服务&&python -m http.server 3500"
-start cmd /k "cd/d D:\2022\霍家营街道\问卷\dingshi&&title 打开浏览器&&node openBrowser.js&&exit" 
+start cmd /k "cd /d e:\2022\霍家营街道\问卷\dingshi&&title 接口服务&&node app.js" 
+start cmd /k "cd /d e:\2022\霍家营街道\问卷\dingshi&&title 页面服务&&python -m http.server 3500"
+start cmd /k "cd /d e:\2022\霍家营街道\问卷\dingshi&&title 打开浏览器&&node openBrowser.js&&exit" 
